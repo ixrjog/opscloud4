67ee175111be9d7d34efbb7e809dd4ec0de915c8
白衣 <baiyi@gegejia.com>
2020-03-18 09:10:36 +0800

    代码复制脚本

--- cp-github.sh
+++ cp-github.sh
@@ -1,2 +1,16 @@
+
 \cp -r ./opscloud-* /Users/liangjian/Documents/workspace/baiyi/opscloud-github/
 \cp .gitignore /Users/liangjian/Documents/workspace/baiyi/opscloud-github/
+\cp -r .idea /Users/liangjian/Documents/workspace/baiyi/opscloud-github/
+\cp -r .mvn /Users/liangjian/Documents/workspace/baiyi/opscloud-github/
+\cp pom.xml /Users/liangjian/Documents/workspace/baiyi/opscloud-github/
+\cp README.md /Users/liangjian/Documents/workspace/baiyi/opscloud-github/
+\cp opscloud.iml /Users/liangjian/Documents/workspace/baiyi/opscloud-github/
+
+\cp -r ./opscloud-* /Users/liangjian/Documents/workspace/baiyi/opscloud-gitee/
+\cp .gitignore /Users/liangjian/Documents/workspace/baiyi/opscloud-gitee/
+\cp -r .idea /Users/liangjian/Documents/workspace/baiyi/opscloud-gitee/
+\cp -r .mvn /Users/liangjian/Documents/workspace/baiyi/opscloud-gitee/
+\cp pom.xml /Users/liangjian/Documents/workspace/baiyi/opscloud-gitee/
+\cp README.md /Users/liangjian/Documents/workspace/baiyi/opscloud-gitee/
+\cp opscloud.iml /Users/liangjian/Documents/workspace/baiyi/opscloud-gitee/
