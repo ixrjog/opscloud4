0d6a1d47bf729e8ac274d105c1082136427f2a6a
白衣 <baiyi@gegejia.com>
2020-05-11 15:58:56 +0800

    Revert "xxx"
    This reverts commit e4543678ae016b3f28c5873115006dcfba516b44.

--- opscloud-manage/src/main/java/com/baiyi/opscloud/factory/xterm/impl/XTermInitialProcess.java
+++ opscloud-manage/src/main/java/com/baiyi/opscloud/factory/xterm/impl/XTermInitialProcess.java
@@ -5,17 +5,16 @@ import com.baiyi.opscloud.domain.BusinessWrapper;
 import com.baiyi.opscloud.domain.bo.SSHKeyCredential;
 import com.baiyi.opscloud.domain.generator.opscloud.OcUser;
 import com.baiyi.opscloud.factory.xterm.IXTermProcess;
-import com.baiyi.opscloud.xterm.handler.ConnectionSession;
 import com.baiyi.opscloud.xterm.handler.RemoteInvokeHandler;
 import com.baiyi.opscloud.xterm.message.BaseXTermWSMessage;
 import com.baiyi.opscloud.xterm.message.XTermInitialWSMessage;
 import com.baiyi.opscloud.xterm.model.HostSystem;
+import com.baiyi.opscloud.xterm.task.SentOutputTask;
 import com.google.gson.GsonBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import javax.websocket.Session;
-import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -54,34 +53,10 @@ public class XTermInitialProcess extends BaseXTermProcess implements IXTermProce
             hostSystem.setSshKeyCredential(sshKeyCredential);
             hostSystem.setInitialMessage(initialMessage);
 
-            ConnectionSession connectionSession =  RemoteInvokeHandler.getSession( instanceId, hostSystem);
-//            Runnable run = new SentOutputTask(sessionId, session);
-//            Thread thread = new Thread(run);
-//            thread.start();
-
-            if (connectionSession != null) {
-                connectionSessionMap.put(session.getId(), connectionSession);
-
-                //WSResult loginSuccess = new WSResult(socketContext.getId(), "", WSContentType.loginSuccss.getCode());
-                //doWriteToSession(session, loginSuccess);
-                connectionSession.registerRead(socketContext.getId(), new SessionBridge() {
-                    @Override
-                    public void process(String value) {
-                        try {
-                            WSResult WSResult = new WSResult(socketContext.getId(), value, com.sdg.cmdb.domain.keybox.WSContentType.response.getCode());
-                            doWriteToSession(session, WSResult);
-                        } catch (IOException e) {
-                            logger.error(e.getMessage(), e);
-                        }
-                    }
-                });
-            } else {
-                logger.warn(socketContext.getId() + " do ssh connection for " + hostSystem.getHost() + " error with:" + hostSystem.getErrorMsg());
-
-                WSResult WSResult = new WSResult(socketContext.getId(), hostSystem.getErrorMsg(), WSContentType.loginFailure.getCode());
-                doWriteToSession(session, WSResult);
-            }
-
+            RemoteInvokeHandler.getSession(sessionId, instanceId, hostSystem);
+            Runnable run = new SentOutputTask(sessionId, session);
+            Thread thread = new Thread(run);
+            thread.start();
         }
     }
 
