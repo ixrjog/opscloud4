78f3ccb2d5f516e70dfb162ed06b2831b0b97dae
修远 <xiuyuan@xinc818.group>
2021-07-27 13:44:56 +0800

    commit

---
+++ opscloud-domain/src/main/java/com/baiyi/opscloud/domain/model/property/ServerProperty.java
@@ -0,0 +1,39 @@
+package com.baiyi.opscloud.domain.model.property;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Data;
+
+import java.util.Map;
+
+/**
+ * @Author <a href="mailto:xiuyuan@xinc818.group">修远</a>
+ * @Date 2021/7/27 10:38 上午
+ * @Since 1.0
+ */
+
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "version",
+        "kind",
+        "metadata",
+        "spec"
+})
+public class ServerProperty {
+
+
+    @JsonProperty("version")
+    private String version = "v1";
+
+    @JsonProperty("kind")
+    private String kind = "server";
+
+    @JsonProperty("metadata")
+    private Map<String, String> metadata;
+
+    @JsonProperty("spec")
+    private Map<String, Object> spec;
+}
