d1d96bc16c89ff68c101a5413d2866178e86d0d4
白衣 <ixrjog@qq.com>
2021-11-05 17:10:52 +0800

    io.fabric8版本升级 5.4.1 => 5.9.0

--- opscloud-manage/src/test/java/com/baiyi/opscloud/ssh/TableTest.java
+++ opscloud-manage/src/test/java/com/baiyi/opscloud/ssh/TableTest.java
@@ -1,12 +1,6 @@
 package com.baiyi.opscloud.ssh;
 
 import com.baiyi.opscloud.BaseUnit;
-import com.baiyi.opscloud.sshcore.table.PrettyTable;
-import com.baiyi.opscloud.sshcore.table.simple.DefaultTable;
-import com.baiyi.opscloud.sshcore.table.simple.DefaultTableFormatter;
-import com.baiyi.opscloud.sshserver.PromptColor;
-import com.baiyi.opscloud.sshserver.SshShellHelper;
-import org.junit.jupiter.api.Test;
 
 
 /**
@@ -16,36 +10,36 @@ import org.junit.jupiter.api.Test;
  */
 public class TableTest extends BaseUnit {
 
-    @Test
-    void tableTest2() {
-        DefaultTable dt = new DefaultTable();
-        dt.setTitle("AAA");
-        dt.setHeaders(new String[]{"Id", "Ok", "Grade", "Remark"});
-        // SshShellHelper.getColoredMessage("Jack Bower", PromptColor.RED),
-        dt.addRow(new Object[]{1001, "Jack Bower", "A", "Good at fighting22222222222222222222222222222"});
-        dt.addRow(new Object[]{1002, "Vincent Willem  Van Gogh", "A+", "Good at painting11111111111111111111111111111111"});
-        dt.addRow(new Object[]{1003, "Jone Doe", "B", "Noop"});
-        DefaultTableFormatter dtf = new DefaultTableFormatter(144, 2);
-        dtf.setIndent("");
-        dtf.setSort(true);
-        System.out.println(dtf.format(dt));
-    }
-
-    @Test
-    void tableTest() {
-        PrettyTable pt = PrettyTable
-                .fieldNames("name", "age", "city");
-
-        // [32mnew york[0m
-        // [31mnew york[0m  8char
-        String s = SshShellHelper.getColoredMessage("new york", PromptColor.RED);
-        // System.out.println(s);
-        pt.addRow(SshShellHelper.getColoredMessage("john", PromptColor.RED) + "," + SshShellHelper.getColoredMessage("b", PromptColor.GREEN), 22, SshShellHelper.getColoredMessage("new york", PromptColor.GREEN))
-                .addRow("elizabeth", 43, "chicago")
-                .addRow("bill", 31, "atlanta")
-                .addRow("mary", 18, "los angeles");
-        System.out.println(pt);
-
-
-    }
+//    @Test
+//    void tableTest2() {
+//        DefaultTable dt = new DefaultTable();
+//        dt.setTitle("AAA");
+//        dt.setHeaders(new String[]{"Id", "Ok", "Grade", "Remark"});
+//        // SshShellHelper.getColoredMessage("Jack Bower", PromptColor.RED),
+//        dt.addRow(new Object[]{1001, "Jack Bower", "A", "Good at fighting22222222222222222222222222222"});
+//        dt.addRow(new Object[]{1002, "Vincent Willem  Van Gogh", "A+", "Good at painting11111111111111111111111111111111"});
+//        dt.addRow(new Object[]{1003, "Jone Doe", "B", "Noop"});
+//        DefaultTableFormatter dtf = new DefaultTableFormatter(144, 2);
+//        dtf.setIndent("");
+//        dtf.setSort(true);
+//        System.out.println(dtf.format(dt));
+//    }
+//
+//    @Test
+//    void tableTest() {
+//        PrettyTable pt = PrettyTable
+//                .fieldNames("name", "age", "city");
+//
+//        // [32mnew york[0m
+//        // [31mnew york[0m  8char
+//        String s = SshShellHelper.getColoredMessage("new york", PromptColor.RED);
+//        // System.out.println(s);
+//        pt.addRow(SshShellHelper.getColoredMessage("john", PromptColor.RED) + "," + SshShellHelper.getColoredMessage("b", PromptColor.GREEN), 22, SshShellHelper.getColoredMessage("new york", PromptColor.GREEN))
+//                .addRow("elizabeth", 43, "chicago")
+//                .addRow("bill", 31, "atlanta")
+//                .addRow("mary", 18, "los angeles");
+//        System.out.println(pt);
+//
+//
+//    }
 }
