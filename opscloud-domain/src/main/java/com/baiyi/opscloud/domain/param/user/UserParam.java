e5128ce691356235b05b735d5d2cc7ac8203037c
白衣 <ixrjog@qq.com>
2021-11-23 10:53:34 +0800

    用户管理支持过滤系统用户，基于System标签

--- opscloud-domain/src/main/java/com/baiyi/opscloud/domain/param/user/UserParam.java
+++ opscloud-domain/src/main/java/com/baiyi/opscloud/domain/param/user/UserParam.java
@@ -2,6 +2,8 @@ package com.baiyi.opscloud.domain.param.user;
 
 import com.baiyi.opscloud.domain.param.IExtend;
 import com.baiyi.opscloud.domain.param.PageParam;
+import com.baiyi.opscloud.domain.types.BusinessTypeEnum;
+import com.baiyi.opscloud.domain.vo.business.BaseBusiness;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
@@ -19,10 +21,18 @@ public class UserParam {
     @EqualsAndHashCode(callSuper = true)
     @NoArgsConstructor
     @ApiModel
-    public static class UserPageQuery extends PageParam implements IExtend {
+    public static class UserPageQuery extends PageParam implements BaseBusiness.IBusinessType, IExtend {
+
+        private final Integer businessType = BusinessTypeEnum.USER.getType();
+
+        // 系统用户标签名称
+        private final String SYSTEM_TAG = "System";
 
         @ApiModelProperty(value = "模糊查询")
         private String queryName;
+
+        @ApiModelProperty(value = "过滤系统用户")
+        private Boolean filterSystemUser;
 
         @ApiModelProperty(value = "展开")
         private Boolean extend;
