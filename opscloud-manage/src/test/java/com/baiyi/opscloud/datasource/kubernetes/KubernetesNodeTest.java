817cd3554044c18abcb5a2ebb15aad2b557bb4c1
白衣 <ixrjog@qq.com>
2021-11-24 18:27:01 +0800

    k8s增加node数据源

---
+++ opscloud-manage/src/test/java/com/baiyi/opscloud/datasource/kubernetes/KubernetesNodeTest.java
@@ -0,0 +1,23 @@
+package com.baiyi.opscloud.datasource.kubernetes;
+
+import com.baiyi.opscloud.datasource.kubernetes.base.BaseKubernetesTest;
+import com.baiyi.opscloud.datasource.kubernetes.datasource.KubernetesNodeDatasource;
+import io.fabric8.kubernetes.api.model.Node;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+/**
+ * @Author baiyi
+ * @Date 2021/11/24 3:08 下午
+ * @Version 1.0
+ */
+public class KubernetesNodeTest extends BaseKubernetesTest {
+
+    @Test
+    void listNodeTest() {
+        List<Node> nodes = KubernetesNodeDatasource.listNode(getConfig().getKubernetes());
+        print(nodes);
+    }
+
+}
---
+++ opscloud-manage/src/test/java/com/baiyi/opscloud/datasource/kubernetes/base/BaseKubernetesTest.java
@@ -0,0 +1,24 @@
+package com.baiyi.opscloud.datasource.kubernetes.base;
+
+import com.baiyi.opscloud.BaseUnit;
+import com.baiyi.opscloud.common.constant.enums.DsTypeEnum;
+import com.baiyi.opscloud.common.datasource.KubernetesConfig;
+import com.baiyi.opscloud.core.factory.DsConfigHelper;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author baiyi
+ * @Date 2021/11/24 3:07 下午
+ * @Version 1.0
+ */
+public class BaseKubernetesTest extends BaseUnit {
+
+    @Resource
+    private DsConfigHelper dsConfigHelper;
+
+    protected KubernetesConfig getConfig() {
+        return dsConfigHelper.build(dsConfigHelper.getConfigByDsType(DsTypeEnum.KUBERNETES.getType()), KubernetesConfig.class);
+    }
+
+}
