43f3b0dc80c83307cb2c31eff56e486a8ff3f0e0
白衣 <ixrjog@qq.com>
2021-12-20 13:34:00 +0800

    Web批量终端使用线程池，4并发初始化登录状态

--- opscloud-terminal/src/main/java/com/baiyi/opscloud/terminal/factory/impl/LoginProcess.java
+++ opscloud-terminal/src/main/java/com/baiyi/opscloud/terminal/factory/impl/LoginProcess.java
@@ -7,19 +7,26 @@ import com.baiyi.opscloud.sshcore.enums.MessageState;
 import com.baiyi.opscloud.sshcore.handler.RemoteInvokeHandler;
 import com.baiyi.opscloud.sshcore.message.ServerMessage;
 import com.baiyi.opscloud.sshcore.model.HostSystem;
+import com.baiyi.opscloud.sshcore.model.ServerNode;
 import com.baiyi.opscloud.terminal.factory.AbstractServerTerminalProcess;
 import com.google.gson.GsonBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.Session;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @Author baiyi
  * @Date 2020/5/11 9:36 上午
  * @Version 1.0
  */
+@Slf4j
 @Component
 public class LoginProcess extends AbstractServerTerminalProcess<ServerMessage.Login>  {
+
+    private static final ExecutorService executor = Executors.newFixedThreadPool(4);
 
     /**
      * 登录
@@ -35,11 +42,14 @@ public class LoginProcess extends AbstractServerTerminalProcess<ServerMessage.Lo
     public void process(String message, Session session, TerminalSession terminalSession) {
         ServerMessage.Login loginMessage = getMessage(message);
         heartbeat(terminalSession.getSessionId());
-        loginMessage.getServerNodes().forEach(serverNode -> {
-            HostSystem hostSystem = hostSystemHandler.buildHostSystem(serverNode, loginMessage);
-            RemoteInvokeHandler.openWebTerminal(terminalSession.getSessionId(), serverNode.getInstanceId(), hostSystem);
-            terminalSessionInstanceService.add(TerminalSessionInstanceBuilder.build(terminalSession, hostSystem, InstanceSessionTypeEnum.SERVER));
-        });
+        for (ServerNode serverNode : loginMessage.getServerNodes()) {
+            executor.submit(() -> {
+                log.info("初始化serverNode: instanceId = {} ",serverNode.getInstanceId());
+                HostSystem hostSystem = hostSystemHandler.buildHostSystem(serverNode, loginMessage);
+                RemoteInvokeHandler.openWebTerminal(terminalSession.getSessionId(), serverNode.getInstanceId(), hostSystem);
+                terminalSessionInstanceService.add(TerminalSessionInstanceBuilder.build(terminalSession, hostSystem, InstanceSessionTypeEnum.SERVER));
+            });
+        }
     }
 
     @Override
