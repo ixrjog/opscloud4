62b850308d53cf5010c453265571b0e7f9595c61
白衣 <ixrjog@qq.com>
2021-12-29 10:42:15 +0800

    优化代码

--- opscloud-datasource-zabbix/src/main/java/com/baiyi/opscloud/zabbix/helper/impl/ZabbixGroupHelperImpl.java
+++ opscloud-datasource-zabbix/src/main/java/com/baiyi/opscloud/zabbix/helper/impl/ZabbixGroupHelperImpl.java
@@ -1,14 +1,13 @@
 package com.baiyi.opscloud.zabbix.helper.impl;
 
 import com.baiyi.opscloud.common.datasource.ZabbixConfig;
-import com.baiyi.opscloud.zabbix.helper.ZabbixGroupHelper;
 import com.baiyi.opscloud.zabbix.ZabbixUtil;
+import com.baiyi.opscloud.zabbix.helper.ZabbixGroupHelper;
 import com.baiyi.opscloud.zabbix.v5.drive.ZabbixV5HostGroupDrive;
 import com.baiyi.opscloud.zabbix.v5.drive.ZabbixV5UserGroupDrive;
 import com.baiyi.opscloud.zabbix.v5.entity.ZabbixUserGroup;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
 
 /**
  * @Author baiyi
@@ -16,13 +15,12 @@ import javax.annotation.Resource;
  * @Version 1.0
  */
 @Component
+@RequiredArgsConstructor
 public class ZabbixGroupHelperImpl implements ZabbixGroupHelper {
 
-    @Resource
-    private ZabbixV5UserGroupDrive zabbixV5UserGroupDatasource;
+    private final ZabbixV5UserGroupDrive zabbixV5UserGroupDrive;
 
-    @Resource
-    private ZabbixV5HostGroupDrive zabbixV5HostGroupDatasource;
+    private final ZabbixV5HostGroupDrive zabbixV5HostGroupDrive;
 
     /**
      * 查询并创建用户组
@@ -33,19 +31,19 @@ public class ZabbixGroupHelperImpl implements ZabbixGroupHelper {
      */
     @Override
     public ZabbixUserGroup.UserGroup getOrCreateUserGroup(ZabbixConfig.Zabbix zabbix, String usergroupName) {
-        com.baiyi.opscloud.zabbix.v5.entity.ZabbixUserGroup.UserGroup userGroup = zabbixV5UserGroupDatasource.getByName(zabbix, usergroupName);
+        com.baiyi.opscloud.zabbix.v5.entity.ZabbixUserGroup.UserGroup userGroup = zabbixV5UserGroupDrive.getByName(zabbix, usergroupName);
         if (userGroup != null) return userGroup;
         // 用户组不存在
         String hostgroup = ZabbixUtil.toHostgroupName(usergroupName);
         com.baiyi.opscloud.zabbix.v5.entity.ZabbixHostGroup.HostGroup hostGroup = getOrCreateHostGroup(zabbix, hostgroup);
-        return zabbixV5UserGroupDatasource.create(zabbix, usergroupName, hostGroup);
+        return zabbixV5UserGroupDrive.create(zabbix, usergroupName, hostGroup);
     }
 
     @Override
     public com.baiyi.opscloud.zabbix.v5.entity.ZabbixHostGroup.HostGroup getOrCreateHostGroup(ZabbixConfig.Zabbix zabbix, String hostgroupName) {
-        com.baiyi.opscloud.zabbix.v5.entity.ZabbixHostGroup.HostGroup hostGroup = zabbixV5HostGroupDatasource.getByName(zabbix, hostgroupName);
+        com.baiyi.opscloud.zabbix.v5.entity.ZabbixHostGroup.HostGroup hostGroup = zabbixV5HostGroupDrive.getByName(zabbix, hostgroupName);
         if (hostGroup != null) return hostGroup;
-        zabbixV5HostGroupDatasource.create(zabbix, hostgroupName);
-        return zabbixV5HostGroupDatasource.getByName(zabbix, hostgroupName);
+        zabbixV5HostGroupDrive.create(zabbix, hostgroupName);
+        return zabbixV5HostGroupDrive.getByName(zabbix, hostgroupName);
     }
 }
