e9d7b2e5d74162fdd49c368d39eb69a69ca91168
修远 <448613222@qq.com>
2022-01-20 16:02:08 +0800

    优化 quartzService

--- opscloud-manage/src/main/java/com/baiyi/opscloud/task/service/QuartzService.java
+++ opscloud-manage/src/main/java/com/baiyi/opscloud/task/service/QuartzService.java
@@ -1,13 +1,18 @@
 package com.baiyi.opscloud.task.service;
 
+import com.baiyi.opscloud.task.ScheduleJob;
 import org.quartz.*;
 import org.quartz.impl.matchers.GroupMatcher;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @Author 修远
@@ -33,65 +38,27 @@ public class QuartzService {
     /**
      * 增加一个job
      *
-     * @param jobClass     任务实现类
-     * @param jobName      任务名称
-     * @param jobGroupName 任务组名
-     * @param jobTime      时间表达式 (这是每隔多少秒为一次任务)
-     * @param jobTimes     运行的次数 （<0:表示不限次数）
-     * @param jobData      参数
+     * @param jobClass 任务实现类
+     * @param jobName  任务名称(建议唯一)
+     * @param group    任务组名
+     * @param jobTime  时间表达式 （如：0/5 * * * * ? ）
+     * @param jobData  参数
      */
-    public void addJob(Class<? extends QuartzJobBean> jobClass, String jobName, String jobGroupName, int jobTime,
-                       int jobTimes, Map jobData) {
-        try {
-            // 任务名称和组构成任务key
-            JobDetail jobDetail = JobBuilder.newJob(jobClass).withIdentity(jobName, jobGroupName)
-                    .build();
-            // 设置job参数
-            if (jobData != null && jobData.size() > 0) {
-                jobDetail.getJobDataMap().putAll(jobData);
-            }
-            // 使用simpleTrigger规则
-            Trigger trigger = null;
-            if (jobTimes < 0) {
-                trigger = TriggerBuilder.newTrigger().withIdentity(jobName, jobGroupName)
-                        .withSchedule(SimpleScheduleBuilder.repeatSecondlyForever(1).withIntervalInSeconds(jobTime))
-                        .startNow().build();
-            } else {
-                trigger = TriggerBuilder
-                        .newTrigger().withIdentity(jobName, jobGroupName).withSchedule(SimpleScheduleBuilder
-                                .repeatSecondlyForever(1).withIntervalInSeconds(jobTime).withRepeatCount(jobTimes))
-                        .startNow().build();
-            }
-            scheduler.scheduleJob(jobDetail, trigger);
-        } catch (SchedulerException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * 增加一个job
-     *
-     * @param jobClass     任务实现类
-     * @param jobName      任务名称(建议唯一)
-     * @param jobGroupName 任务组名
-     * @param jobTime      时间表达式 （如：0/5 * * * * ? ）
-     * @param jobData      参数
-     */
-    public void addJob(Class<? extends QuartzJobBean> jobClass, String jobName, String jobGroupName, String jobTime, Map jobData) {
+    public void addJob(Class<? extends QuartzJobBean> jobClass, String jobName, String group, String jobTime, Map jobData) {
         try {
             // 创建jobDetail实例，绑定Job实现类
             // 指明job的名称，所在组的名称，以及绑定job类
             // 任务名称和组构成任务key
-            JobDetail jobDetail = JobBuilder.newJob(jobClass).withIdentity(jobName, jobGroupName)
+            JobDetail jobDetail = JobBuilder.newJob(jobClass).withIdentity(jobName, group)
                     .build();
             // 设置job参数
-            if (jobData != null && jobData.size() > 0) {
+            if (!CollectionUtils.isEmpty(jobData)) {
                 jobDetail.getJobDataMap().putAll(jobData);
             }
             // 定义调度触发规则
             // 使用cornTrigger规则
             // 触发器key
-            Trigger trigger = TriggerBuilder.newTrigger().withIdentity(jobName, jobGroupName)
+            Trigger trigger = TriggerBuilder.newTrigger().withIdentity(jobName, group)
                     .startAt(DateBuilder.futureDate(1, DateBuilder.IntervalUnit.SECOND))
                     .withSchedule(CronScheduleBuilder.cronSchedule(jobTime)).startNow().build();
             // 把作业和触发器注册到任务调度中
@@ -105,12 +72,12 @@ public class QuartzService {
      * 修改 一个job的 时间表达式
      *
      * @param jobName
-     * @param jobGroupName
+     * @param group
      * @param jobTime
      */
-    public void updateJob(String jobName, String jobGroupName, String jobTime) {
+    public void updateJob(String jobName, String group, String jobTime) {
         try {
-            TriggerKey triggerKey = TriggerKey.triggerKey(jobName, jobGroupName);
+            TriggerKey triggerKey = TriggerKey.triggerKey(jobName, group);
             CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);
             trigger = trigger.getTriggerBuilder().withIdentity(triggerKey)
                     .withSchedule(CronScheduleBuilder.cronSchedule(jobTime)).build();
@@ -122,15 +89,25 @@ public class QuartzService {
     }
 
     /**
-     * 删除任务一个job
-     *
-     * @param jobName      任务名称
-     * @param jobGroupName 任务组名
+     * @param jobName
+     * @param group
      */
-    public void deleteJob(String jobName, String jobGroupName) {
+    public void deleteJob(String jobName, String group) {
+        TriggerKey triggerKey = TriggerKey.triggerKey(
+                jobName, group);
+        JobKey jobKey = JobKey.jobKey(jobName, group);
         try {
-            scheduler.deleteJob(new JobKey(jobName, jobGroupName));
-        } catch (Exception e) {
+            Trigger trigger = scheduler.getTrigger(triggerKey);
+            if (trigger == null) {
+                return;
+            }
+            // 停止触发器
+            scheduler.pauseTrigger(triggerKey);
+            // 移除触发器
+            scheduler.unscheduleJob(triggerKey);
+            // 删除任务
+            scheduler.deleteJob(jobKey);
+        } catch (SchedulerException e) {
             e.printStackTrace();
         }
     }
@@ -139,11 +116,11 @@ public class QuartzService {
      * 暂停一个job
      *
      * @param jobName
-     * @param jobGroupName
+     * @param group
      */
-    public void pauseJob(String jobName, String jobGroupName) {
+    public void pauseJob(String jobName, String group) {
         try {
-            JobKey jobKey = JobKey.jobKey(jobName, jobGroupName);
+            JobKey jobKey = JobKey.jobKey(jobName, group);
             scheduler.pauseJob(jobKey);
         } catch (SchedulerException e) {
             e.printStackTrace();
@@ -154,11 +131,11 @@ public class QuartzService {
      * 恢复一个job
      *
      * @param jobName
-     * @param jobGroupName
+     * @param group
      */
-    public void resumeJob(String jobName, String jobGroupName) {
+    public void resumeJob(String jobName, String group) {
         try {
-            JobKey jobKey = JobKey.jobKey(jobName, jobGroupName);
+            JobKey jobKey = JobKey.jobKey(jobName, group);
             scheduler.resumeJob(jobKey);
         } catch (SchedulerException e) {
             e.printStackTrace();
@@ -169,11 +146,11 @@ public class QuartzService {
      * 立即执行一个job
      *
      * @param jobName
-     * @param jobGroupName
+     * @param group
      */
-    public void runJobNow(String jobName, String jobGroupName) {
+    public void runJobNow(String jobName, String group) {
         try {
-            JobKey jobKey = JobKey.jobKey(jobName, jobGroupName);
+            JobKey jobKey = JobKey.jobKey(jobName, group);
             scheduler.triggerJob(jobKey);
         } catch (SchedulerException e) {
             e.printStackTrace();
@@ -184,65 +161,57 @@ public class QuartzService {
      * 获取所有计划中的任务列表
      *
      * @return
+     * @throws SchedulerException
      */
-    public List<Map<String, Object>> queryAllJob() {
-        List<Map<String, Object>> jobList = null;
-        try {
-            GroupMatcher<JobKey> matcher = GroupMatcher.anyJobGroup();
-            Set<JobKey> jobKeys = scheduler.getJobKeys(matcher);
-            jobList = new ArrayList<Map<String, Object>>();
-            for (JobKey jobKey : jobKeys) {
-                List<? extends Trigger> triggers = scheduler.getTriggersOfJob(jobKey);
-                for (Trigger trigger : triggers) {
-                    Map<String, Object> map = new HashMap<>();
-                    map.put("jobName", jobKey.getName());
-                    map.put("jobGroupName", jobKey.getGroup());
-                    map.put("description", "触发器:" + trigger.getKey());
-                    Trigger.TriggerState triggerState = scheduler.getTriggerState(trigger.getKey());
-                    map.put("jobStatus", triggerState.name());
-                    if (trigger instanceof CronTrigger) {
-                        CronTrigger cronTrigger = (CronTrigger) trigger;
-                        String cronExpression = cronTrigger.getCronExpression();
-                        map.put("jobTime", cronExpression);
-                    }
-                    jobList.add(map);
+    public List<ScheduleJob> getAllJob() throws SchedulerException {
+        GroupMatcher<JobKey> matcher = GroupMatcher.anyJobGroup();
+        Set<JobKey> jobKeys = scheduler.getJobKeys(matcher);
+        List<ScheduleJob> jobList = new ArrayList<ScheduleJob>();
+        for (JobKey jobKey : jobKeys) {
+            List<? extends Trigger> triggers = scheduler.getTriggersOfJob(jobKey);
+            for (Trigger trigger : triggers) {
+                Trigger.TriggerState triggerState = scheduler.getTriggerState(trigger.getKey());
+                ScheduleJob job = ScheduleJob.builder()
+                        .name(jobKey.getName())
+                        .group(jobKey.getGroup())
+                        .description("触发器: " + trigger.getKey())
+                        .status(triggerState.name())
+                        .build();
+                if (trigger instanceof CronTrigger) {
+                    CronTrigger cronTrigger = (CronTrigger) trigger;
+                    job.setCronExpression(cronTrigger.getCronExpression());
                 }
+                jobList.add(job);
             }
-        } catch (SchedulerException e) {
-            e.printStackTrace();
         }
         return jobList;
     }
 
     /**
-     * 获取所有正在运行的job
+     * 所有正在运行的job
      *
      * @return
+     * @throws SchedulerException
      */
-    public List<Map<String, Object>> queryRunJob() {
-        List<Map<String, Object>> jobList = null;
-        try {
-            List<JobExecutionContext> executingJobs = scheduler.getCurrentlyExecutingJobs();
-            jobList = new ArrayList<Map<String, Object>>(executingJobs.size());
-            for (JobExecutionContext executingJob : executingJobs) {
-                Map<String, Object> map = new HashMap<String, Object>();
-                JobDetail jobDetail = executingJob.getJobDetail();
-                JobKey jobKey = jobDetail.getKey();
-                Trigger trigger = executingJob.getTrigger();
-                map.put("jobName", jobKey.getName());
-                map.put("jobGroupName", jobKey.getGroup());
-                map.put("description", "触发器:" + trigger.getKey());
-                Trigger.TriggerState triggerState = scheduler.getTriggerState(trigger.getKey());
-                map.put("jobStatus", triggerState.name());
-                if (trigger instanceof CronTrigger) {
-                    CronTrigger cronTrigger = (CronTrigger) trigger;
-                    String cronExpression = cronTrigger.getCronExpression();
-                    map.put("jobTime", cronExpression);
-                }
-                jobList.add(map);
+    public List<ScheduleJob> getRunningJob() throws SchedulerException {
+        List<JobExecutionContext> executingJobs = scheduler.getCurrentlyExecutingJobs();
+        List<ScheduleJob> jobList = new ArrayList<ScheduleJob>(executingJobs.size());
+        for (JobExecutionContext executingJob : executingJobs) {
+            JobDetail jobDetail = executingJob.getJobDetail();
+            JobKey jobKey = jobDetail.getKey();
+            Trigger trigger = executingJob.getTrigger();
+            Trigger.TriggerState triggerState = scheduler.getTriggerState(trigger.getKey());
+            ScheduleJob job = ScheduleJob.builder()
+                    .name(jobKey.getName())
+                    .group(jobKey.getGroup())
+                    .description("触发器: " + trigger.getKey())
+                    .status(triggerState.name())
+                    .build();
+            if (trigger instanceof CronTrigger) {
+                CronTrigger cronTrigger = (CronTrigger) trigger;
+                job.setCronExpression(cronTrigger.getCronExpression());
             }
-        } catch (SchedulerException e) {
-            e.printStackTrace();
+            jobList.add(job);
         }
         return jobList;
     }
--- opscloud-manage/src/test/java/com/baiyi/opscloud/config/WhiteConfigTest.java
+++ opscloud-manage/src/test/java/com/baiyi/opscloud/config/WhiteConfigTest.java
@@ -2,11 +2,14 @@ package com.baiyi.opscloud.config;
 
 import com.baiyi.opscloud.BaseUnit;
 import com.baiyi.opscloud.config.properties.WhiteConfigurationProperties;
+import com.baiyi.opscloud.task.ScheduleJob;
 import com.baiyi.opscloud.task.service.QuartzService;
 import org.junit.jupiter.api.Test;
+import org.quartz.SchedulerException;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @Author baiyi
@@ -28,19 +31,21 @@ class WhiteConfigTest extends BaseUnit {
 
 
     @Test
-    void taskTest() {
+    void taskTest() throws SchedulerException {
         HashMap<String, Object> map = new HashMap<>();
         map.put("name", 1);
 //        quartzService.deleteJob("job", "test");
-        quartzService.pauseJob("job", "test");
+//        quartzService.pauseJob("job", "test");
 //        quartzService.addJob(JobTest.class, "job", "test", "*/5 * * * * ?", map);
-        while ( true ) {
-            System.err.println("------");
-            try {
-                Thread.sleep(1000 * 30);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        List<ScheduleJob> list = quartzService.getAllJob();
+        System.err.println(list);
+//        while (true) {
+//            System.err.println("------");
+//            try {
+//                Thread.sleep(1000 * 30);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 }
