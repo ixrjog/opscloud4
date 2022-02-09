6b0c35c8a5defb97b0d15d0626bfff256573a8f6
白衣 <ixrjog@qq.com>
2022-02-09 16:48:27 +0800

    优化标签包装类

--- opscloud-service/src/main/java/com/baiyi/opscloud/packer/tag/TagPacker.java
+++ opscloud-service/src/main/java/com/baiyi/opscloud/packer/tag/TagPacker.java
@@ -1,17 +1,10 @@
 package com.baiyi.opscloud.packer.tag;
 
-import com.baiyi.opscloud.common.util.BeanCopierUtil;
-import com.baiyi.opscloud.domain.generator.opscloud.Tag;
-import com.baiyi.opscloud.domain.param.tag.TagParam;
 import com.baiyi.opscloud.domain.constants.BusinessTypeEnum;
 import com.baiyi.opscloud.domain.vo.tag.TagVO;
 import com.baiyi.opscloud.service.tag.BusinessTagService;
-import com.baiyi.opscloud.service.tag.TagService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @Author baiyi
@@ -22,30 +15,11 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class TagPacker {
 
-    private final TagService tagService;
-
     private final BusinessTagService businessTagService;
 
-    private List<Tag> queryBusinessTagByParam(TagParam.BusinessQuery queryParam) {
-        return tagService.queryBusinessTagByParam(queryParam);
-    }
-
-    public void wrap(TagVO.ITags iTags) {
-        TagParam.BusinessQuery queryParam = TagParam.BusinessQuery.builder()
-                .businessType(iTags.getBusinessType())
-                .businessId(iTags.getBusinessId())
-                .build();
-        List<Tag> tags = queryBusinessTagByParam(queryParam);
-        iTags.setTags(wrapVOList(tags));
-    }
-
-    public List<TagVO.Tag> wrapVOList(List<Tag> data) {
-        return data.stream().map(e -> {
-            TagVO.Tag tag = BeanCopierUtil.copyProperties(e, TagVO.Tag.class);
-            tag.setBusinessTypeEnum(BusinessTypeEnum.getByType(e.getBusinessType()));
-            tag.setQuantityUsed(businessTagService.countByTagId(tag.getId()));
-            return tag;
-        }).collect(Collectors.toList());
+    public void wrap(TagVO.Tag tag){
+        tag.setBusinessTypeEnum(BusinessTypeEnum.getByType(tag.getBusinessType()));
+        tag.setQuantityUsed(businessTagService.countByTagId(tag.getId()));
     }
 
 }
