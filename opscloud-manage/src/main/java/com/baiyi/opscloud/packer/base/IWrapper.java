7acf89bffcf5d751afd4428789d9b61dea2869f4
白衣 <ixrjog@qq.com>
2022-02-18 10:16:07 +0800

    钉钉同步用户代码优化

---
+++ opscloud-manage/src/main/java/com/baiyi/opscloud/packer/base/IWrapper.java
@@ -0,0 +1,8 @@
+package com.baiyi.opscloud.packer.base;
+/**
+ * @Author  baiyi
+ * @Date  2022/2/17 3:42 PM
+ * @Version 1.0
+ */
+public class IWrapper {
+}
---
+++ opscloud-service/src/main/java/com/baiyi/opscloud/packer/ServerPackerDelegate.java
@@ -0,0 +1,8 @@
+package com.baiyi.opscloud.packer;
+/**
+ * @Author  baiyi
+ * @Date  2022/2/17 5:23 PM
+ * @Version 1.0
+ */
+public class SshServerPackerD {
+}
