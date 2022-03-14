82ae39ae8a07dcad38e398792ea317be7870459a
白衣 <ixrjog@qq.com>
2022-03-14 17:16:23 +0800

    代码优化

--- opscloud-datasource/src/main/java/com/baiyi/opscloud/datasource/business/server/util/HostParamUtil.java
+++ opscloud-datasource/src/main/java/com/baiyi/opscloud/datasource/business/server/util/HostParamUtil.java
@@ -3,7 +3,6 @@ package com.baiyi.opscloud.datasource.business.server.util;
 import com.baiyi.opscloud.domain.generator.opscloud.Server;
 import com.baiyi.opscloud.domain.model.property.ServerProperty;
 import com.baiyi.opscloud.zabbix.v5.param.ZabbixHostParam;
-import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,7 +15,8 @@ import java.util.stream.Collectors;
  */
 public class HostParamUtil {
 
-    private HostParamUtil(){}
+    private HostParamUtil() {
+    }
 
     public static ZabbixHostParam.Interface buildInterfaceParam(Server server, ServerProperty.Server property) {
         return ZabbixHostParam.Interface.builder()
@@ -25,11 +25,10 @@ public class HostParamUtil {
     }
 
     public static String getManageIp(Server server, ServerProperty.Server property) {
-        String manageIp = Optional.ofNullable(property)
+        return Optional.ofNullable(property)
                 .map(ServerProperty.Server::getMetadata)
                 .map(ServerProperty.Metadata::getManageIp)
                 .orElse(server.getPrivateIp());
-        return StringUtils.isEmpty(manageIp) ? server.getPrivateIp() : manageIp;
     }
 
     public static List<ZabbixHostParam.Template> toTemplateParam(List<com.baiyi.opscloud.zabbix.v5.entity.ZabbixTemplate.Template> templates) {
