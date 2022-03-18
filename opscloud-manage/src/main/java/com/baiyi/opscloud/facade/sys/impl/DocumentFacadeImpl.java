8b8291a5066af13890906d89cbe6381cf0e22042
白衣 <ixrjog@qq.com>
2022-03-18 14:20:46 +0800

    doc支持注入email

--- opscloud-manage/src/main/java/com/baiyi/opscloud/facade/sys/impl/DocumentFacadeImpl.java
+++ opscloud-manage/src/main/java/com/baiyi/opscloud/facade/sys/impl/DocumentFacadeImpl.java
@@ -5,10 +5,12 @@ import com.baiyi.opscloud.common.builder.SimpleDictBuilder;
 import com.baiyi.opscloud.common.util.SessionUtil;
 import com.baiyi.opscloud.common.util.TemplateUtil;
 import com.baiyi.opscloud.domain.generator.opscloud.Document;
+import com.baiyi.opscloud.domain.generator.opscloud.User;
 import com.baiyi.opscloud.domain.param.sys.DocumentParam;
 import com.baiyi.opscloud.domain.vo.sys.DocumentVO;
 import com.baiyi.opscloud.facade.sys.DocumentFacade;
 import com.baiyi.opscloud.service.sys.DocumentService;
+import com.baiyi.opscloud.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +27,8 @@ public class DocumentFacadeImpl implements DocumentFacade {
 
     private final DocumentService documentService;
 
+    private final UserService userService;
+
     @Override
     public DocumentVO.Doc previewDocument(DocumentParam.DocumentQuery query) {
         Document doc = documentService.getByKey(query.getDocumentKey());
@@ -36,9 +40,11 @@ public class DocumentFacadeImpl implements DocumentFacade {
     }
 
     private void render(Document doc, DocumentParam.DocumentQuery query) {
+        User user = userService.getByUsername(SessionUtil.getUsername());
         render(doc, query.getDict()); // 反向注入
         SimpleDict simpleDict = SimpleDictBuilder.newBuilder()
                 .putParam("username", SessionUtil.getUsername())
+                .putParam("email", user.getEmail())
                 .build();
         render(doc, simpleDict.getDict());
     }
