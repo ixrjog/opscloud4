1878373d4088b406dc8c562cd9038bda73f2f6f0
白衣 <ixrjog@qq.com>
2022-03-23 11:27:30 +0800

    ssh-server 增加心跳保持会话

--- opscloud-ssh-server/src/main/java/com/baiyi/opscloud/sshserver/SshShellRunnable.java
+++ opscloud-ssh-server/src/main/java/com/baiyi/opscloud/sshserver/SshShellRunnable.java
@@ -21,6 +21,7 @@ import com.baiyi.opscloud.sshserver.listeners.SshShellListenerService;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.sshd.common.Factory;
+import org.apache.sshd.common.session.SessionHeartbeatController;
 import org.apache.sshd.server.ExitCallback;
 import org.apache.sshd.server.Signal;
 import org.apache.sshd.server.channel.ChannelSession;
@@ -51,6 +52,7 @@ import org.springframework.shell.result.DefaultResultHandler;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 
 import static com.baiyi.opscloud.sshserver.SshShellCommandFactory.SSH_THREAD_CONTEXT;
 import static com.baiyi.opscloud.sshserver.auth.SshShellAuthenticationProvider.AUTHENTICATION_ATTRIBUTE;
@@ -207,6 +209,8 @@ public class SshShellRunnable
                         .build();
 
                 Object authenticationObject = session.getSession().getIoSession().getAttribute(AUTHENTICATION_ATTRIBUTE);
+                // 10秒发送空心跳
+                session.getSession().setSessionHeartbeat(SessionHeartbeatController.HeartbeatType.NONE, Duration.ofSeconds(10));
                 SshAuthentication authentication = null;
                 if (authenticationObject != null) {
                     if (!(authenticationObject instanceof SshAuthentication)) {
