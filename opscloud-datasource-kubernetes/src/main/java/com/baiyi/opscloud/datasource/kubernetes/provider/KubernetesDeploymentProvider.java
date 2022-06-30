171bf81eb821c9eb1df9522705d03e759dee76a0
白衣 <ixrjog@qq.com>
2022-06-30 19:04:02 +0800

    Kubernetes Deployment、Namespace 互为关系

--- opscloud-datasource-kubernetes/src/main/java/com/baiyi/opscloud/datasource/kubernetes/provider/KubernetesDeploymentProvider.java
+++ opscloud-datasource-kubernetes/src/main/java/com/baiyi/opscloud/datasource/kubernetes/provider/KubernetesDeploymentProvider.java
@@ -1,20 +1,20 @@
 package com.baiyi.opscloud.datasource.kubernetes.provider;
 
 import com.baiyi.opscloud.common.annotation.SingleTask;
-import com.baiyi.opscloud.common.datasource.KubernetesConfig;
 import com.baiyi.opscloud.common.constants.enums.DsTypeEnum;
+import com.baiyi.opscloud.common.datasource.KubernetesConfig;
 import com.baiyi.opscloud.core.factory.AssetProviderFactory;
+import com.baiyi.opscloud.core.model.DsInstanceContext;
+import com.baiyi.opscloud.core.provider.asset.AbstractAssetRelationProvider;
+import com.baiyi.opscloud.core.util.AssetUtil;
 import com.baiyi.opscloud.datasource.kubernetes.converter.DeploymentAssetConverter;
 import com.baiyi.opscloud.datasource.kubernetes.driver.KubernetesDeploymentDriver;
 import com.baiyi.opscloud.datasource.kubernetes.driver.KubernetesNamespaceDriver;
-import com.baiyi.opscloud.core.model.DsInstanceContext;
-import com.baiyi.opscloud.core.provider.asset.BaseAssetProvider;
-import com.baiyi.opscloud.core.util.AssetUtil;
 import com.baiyi.opscloud.domain.builder.asset.AssetContainer;
+import com.baiyi.opscloud.domain.constants.DsAssetTypeConstants;
 import com.baiyi.opscloud.domain.generator.opscloud.DatasourceConfig;
 import com.baiyi.opscloud.domain.generator.opscloud.DatasourceInstance;
 import com.baiyi.opscloud.domain.generator.opscloud.DatasourceInstanceAsset;
-import com.baiyi.opscloud.domain.constants.DsAssetTypeConstants;
 import com.google.common.collect.Lists;
 import io.fabric8.kubernetes.api.model.Namespace;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
@@ -27,44 +27,23 @@ import static com.baiyi.opscloud.common.constants.SingleTaskConstants.PULL_KUBER
 
 /**
  * @Author baiyi
- * @Date 2021/6/25 4:20 下午
+ * @Date 2022/6/30 17:55
  * @Version 1.0
  */
 @Component
-public class KubernetesDeploymentProvider extends BaseAssetProvider<Deployment> {
+public class KubernetesDeploymentProvider2 extends AbstractAssetRelationProvider<Deployment, Namespace> {
 
     @Resource
-    private KubernetesDeploymentProvider kubernetesDeploymentProvider;
+    private KubernetesDeploymentProvider2 kubernetesDeploymentProvider2;
 
     @Override
-    public String getInstanceType() {
-        return DsTypeEnum.KUBERNETES.name();
-    }
-
-    @Override
-    public String getAssetType() {
-        return DsAssetTypeConstants.KUBERNETES_DEPLOYMENT.name();
+    @SingleTask(name = PULL_KUBERNETES_DEPLOYMENT, lockTime = "5m")
+    public void pullAsset(int dsInstanceId) {
+        doPull(dsInstanceId);
     }
 
     private KubernetesConfig.Kubernetes buildConfig(DatasourceConfig dsConfig) {
         return dsConfigHelper.build(dsConfig, KubernetesConfig.class).getKubernetes();
-    }
-
-    @Override
-    protected List<Deployment> listEntities(DsInstanceContext dsInstanceContext) {
-        KubernetesConfig.Kubernetes kubernetes = buildConfig(dsInstanceContext.getDsConfig());
-        List<Namespace> namespaces = KubernetesNamespaceDriver.listNamespace(buildConfig(dsInstanceContext.getDsConfig()));
-        List<Deployment> entities = Lists.newArrayList();
-        namespaces.forEach(e ->
-                entities.addAll(KubernetesDeploymentDriver.listDeployment(kubernetes, e.getMetadata().getName()))
-        );
-        return entities;
-    }
-
-    @Override
-    @SingleTask(name = PULL_KUBERNETES_DEPLOYMENT , lockTime = "5m")
-    public void pullAsset(int dsInstanceId) {
-        doPull(dsInstanceId);
     }
 
     @Override
@@ -81,12 +60,46 @@ public class KubernetesDeploymentProvider extends BaseAssetProvider<Deployment> 
     }
 
     @Override
+    protected List<Deployment> listEntities(DsInstanceContext dsInstanceContext) {
+        KubernetesConfig.Kubernetes kubernetes = buildConfig(dsInstanceContext.getDsConfig());
+        List<Namespace> namespaces = KubernetesNamespaceDriver.listNamespace(buildConfig(dsInstanceContext.getDsConfig()));
+        List<Deployment> entities = Lists.newArrayList();
+        namespaces.forEach(e ->
+                entities.addAll(KubernetesDeploymentDriver.listDeployment(kubernetes, e.getMetadata().getName()))
+        );
+        return entities;
+    }
+
+    @Override
+    protected List<Deployment> listEntities(DsInstanceContext dsInstanceContext, Namespace target) {
+        KubernetesConfig.Kubernetes kubernetes = buildConfig(dsInstanceContext.getDsConfig());
+        return KubernetesDeploymentDriver.listDeployment(kubernetes, target.getMetadata().getName());
+    }
+
+    @Override
+    public String getInstanceType() {
+        return DsTypeEnum.KUBERNETES.name();
+    }
+
+    @Override
+    public String getAssetType() {
+        return DsAssetTypeConstants.KUBERNETES_DEPLOYMENT.name();
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        AssetProviderFactory.register(kubernetesDeploymentProvider2);
+    }
+
+    @Override
     protected AssetContainer toAssetContainer(DatasourceInstance dsInstance, Deployment entity) {
         return DeploymentAssetConverter.toAssetContainer(dsInstance, entity);
     }
 
     @Override
-    public void afterPropertiesSet() {
-        AssetProviderFactory.register(kubernetesDeploymentProvider);
+    public String getTargetAssetKey() {
+        return DsAssetTypeConstants.KUBERNETES_NAMESPACE.name();
     }
+
+
 }
--- opscloud-datasource-kubernetes/src/main/java/com/baiyi/opscloud/datasource/kubernetes/provider/KubernetesNamespaceProvider.java
+++ opscloud-datasource-kubernetes/src/main/java/com/baiyi/opscloud/datasource/kubernetes/provider/KubernetesNamespaceProvider.java
@@ -1,81 +1,8 @@
 package com.baiyi.opscloud.datasource.kubernetes.provider;
-
-import com.baiyi.opscloud.common.annotation.SingleTask;
-import com.baiyi.opscloud.common.datasource.KubernetesConfig;
-import com.baiyi.opscloud.common.constants.enums.DsTypeEnum;
-import com.baiyi.opscloud.core.factory.AssetProviderFactory;
-import com.baiyi.opscloud.datasource.kubernetes.converter.NamespaceAssetConverter;
-import com.baiyi.opscloud.datasource.kubernetes.driver.KubernetesNamespaceDriver;
-import com.baiyi.opscloud.core.model.DsInstanceContext;
-import com.baiyi.opscloud.core.provider.asset.BaseAssetProvider;
-import com.baiyi.opscloud.core.util.AssetUtil;
-import com.baiyi.opscloud.domain.builder.asset.AssetContainer;
-import com.baiyi.opscloud.domain.generator.opscloud.DatasourceConfig;
-import com.baiyi.opscloud.domain.generator.opscloud.DatasourceInstance;
-import com.baiyi.opscloud.domain.generator.opscloud.DatasourceInstanceAsset;
-import com.baiyi.opscloud.domain.constants.DsAssetTypeConstants;
-import io.fabric8.kubernetes.api.model.Namespace;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
-import java.util.List;
-
-import static com.baiyi.opscloud.common.constants.SingleTaskConstants.PULL_KUBERNETES_NAMESPACE;
-
 /**
- * @Author baiyi
- * @Date 2021/6/24 9:25 下午
+ * @Author  baiyi
+ * @Date  2022/6/30 18:22
  * @Version 1.0
  */
-@Component
-public class KubernetesNamespaceProvider extends BaseAssetProvider<Namespace> {
-
-    @Resource
-    private KubernetesNamespaceProvider kubernetesNamespaceProvider;
-
-    @Override
-    public String getInstanceType() {
-        return DsTypeEnum.KUBERNETES.name();
-    }
-
-    @Override
-    public String getAssetType() {
-        return DsAssetTypeConstants.KUBERNETES_NAMESPACE.name();
-    }
-
-    private KubernetesConfig.Kubernetes buildConfig(DatasourceConfig dsConfig) {
-        return dsConfigHelper.build(dsConfig, KubernetesConfig.class).getKubernetes();
-    }
-
-    @Override
-    protected List<Namespace> listEntities(DsInstanceContext dsInstanceContext) {
-        return KubernetesNamespaceDriver.listNamespace(buildConfig(dsInstanceContext.getDsConfig()));
-    }
-
-    @Override
-    @SingleTask(name = PULL_KUBERNETES_NAMESPACE)
-    public void pullAsset(int dsInstanceId) {
-        doPull(dsInstanceId);
-    }
-
-    @Override
-    protected boolean equals(DatasourceInstanceAsset asset, DatasourceInstanceAsset preAsset) {
-        if (!AssetUtil.equals(preAsset.getAssetKey(), asset.getAssetKey()))
-            return false;
-        if (!AssetUtil.equals(preAsset.getName(), asset.getName()))
-            return false;
-        if (preAsset.getIsActive() != asset.getIsActive())
-            return false;
-        return true;
-    }
-
-    @Override
-    protected AssetContainer toAssetContainer(DatasourceInstance dsInstance, Namespace entity) {
-        return NamespaceAssetConverter.toAssetContainer(dsInstance, entity);
-    }
-
-    @Override
-    public void afterPropertiesSet() {
-        AssetProviderFactory.register(kubernetesNamespaceProvider);
-    }
+public class KubernetesNamespaceProvider {
 }
