36350b85922a71890b6d14d14c8eb0c61f1aad2d
修远 <448613222@qq.com>
2022-07-23 00:25:49 +0800

    阿里云 语音通知 接入

--- opscloud-manage/src/test/java/com/baiyi/opscloud/datasource/aliyun/AliyunEcrTest.java
+++ opscloud-manage/src/test/java/com/baiyi/opscloud/datasource/aliyun/AliyunEcrTest.java
@@ -4,6 +4,7 @@ import com.aliyuncs.cr.model.v20181201.ListRepositoryResponse;
 import com.baiyi.opscloud.common.datasource.AliyunConfig;
 import com.baiyi.opscloud.datasource.aliyun.acr.driver.AliyunAcrRepositoryDriver;
 import com.baiyi.opscloud.datasource.aliyun.base.BaseAliyunTest;
+import com.baiyi.opscloud.datasource.message.driver.AliyunVoiceNotifyDriver;
 import org.junit.jupiter.api.Test;
 
 import javax.annotation.Resource;
@@ -19,6 +20,9 @@ public class AliyunEcrTest extends BaseAliyunTest {
     @Resource
     private AliyunAcrRepositoryDriver aliyunAcrRepositoryDriver;
 
+    @Resource
+    private AliyunVoiceNotifyDriver aliyunVoiceNotifyDriver;
+
     private final static String instanceId = "cri-4v9b8l2gc3en0x34";
 
     @Test
@@ -27,5 +31,13 @@ public class AliyunEcrTest extends BaseAliyunTest {
         List<ListRepositoryResponse.RepositoriesItem> repositoriesItems = aliyunAcrRepositoryDriver.listRepositories("eu-west-1", config.getAliyun(), instanceId);
         print(repositoriesItems);
     }
+
+    @Test
+    void xx(){
+        AliyunConfig config = getConfig();
+        aliyunVoiceNotifyDriver.singleCallByTts("eu-west-1",config.getAliyun(),"15067127069","TTS_246450043");
+
+    }
+
 
 }
