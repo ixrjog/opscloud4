324ec41ef76d0e08548133d7adb47f3349843cc6
修远 <448613222@qq.com>
2022-09-21 13:55:47 +0800

    告警通知重构

--- opscloud-service/src/main/java/com/baiyi/opscloud/service/alert/AlertNotifyEventService.java
+++ opscloud-service/src/main/java/com/baiyi/opscloud/service/alert/AlertNotifyEventService.java
@@ -2,6 +2,8 @@ package com.baiyi.opscloud.service.alert;
 
 import com.baiyi.opscloud.domain.generator.opscloud.AlertNotifyEvent;
 
+import java.util.List;
+
 /**
  * @Author 修远
  * @Date 2022/9/14 10:32 AM
@@ -12,4 +14,8 @@ public interface AlertNotifyEventService {
     void add(AlertNotifyEvent alertNotifyEvent);
 
     AlertNotifyEvent getByUuid(String eventUuid);
+
+    List<AlertNotifyEvent> listAll();
+
+    List<AlertNotifyEvent> listByService(String service);
 }
