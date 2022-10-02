f8fa1f4e297f6c154e1bb3e015a930e264bb4616
白衣 <ixrjog@qq.com>
2022-10-02 17:39:46 +0800

    限制分页查询上限，避免安全隐患

--- opscloud-domain/src/main/java/com/baiyi/opscloud/domain/param/PageParam.java
+++ opscloud-domain/src/main/java/com/baiyi/opscloud/domain/param/PageParam.java
@@ -4,6 +4,8 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+
 @Data
 @ApiModel
 public class PageParam {
@@ -11,6 +13,7 @@ public class PageParam {
     @ApiModelProperty(value = "分页页码")
     private Integer page;
 
+    @Max(value = 50, message = "分页查询最大限制50条记录")
     @ApiModelProperty(value = "分页页长",example = "10")
     private Integer length;
 
