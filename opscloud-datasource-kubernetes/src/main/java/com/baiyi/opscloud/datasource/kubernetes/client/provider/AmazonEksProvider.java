a0c1b629c8b001fcbe5dbfb81832878af2ede3bc
白衣 <ixrjog@qq.com>
2022-11-03 14:21:21 +0800

    fixbug 回滚依赖io.fabric8.kubernetes 6.2.0 -> 5.12.4

--- opscloud-datasource-kubernetes/src/main/java/com/baiyi/opscloud/datasource/kubernetes/client/provider/AmazonEksProvider.java
+++ opscloud-datasource-kubernetes/src/main/java/com/baiyi/opscloud/datasource/kubernetes/client/provider/AmazonEksProvider.java
@@ -4,8 +4,8 @@ import com.baiyi.opscloud.common.datasource.KubernetesConfig;
 import com.baiyi.opscloud.datasource.kubernetes.client.KubeClient;
 import com.baiyi.opscloud.datasource.kubernetes.client.provider.eks.AmazonEksHelper;
 import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -36,7 +36,7 @@ public class AmazonEksProvider {
      */
     public static KubernetesClient buildWithProviderClient(KubernetesConfig.Kubernetes kubernetes) throws URISyntaxException {
         String token = amazonEksHelper.generateEksToken(kubernetes.getAmazonEks());
-        preInitConfig(kubernetes);
+        initConfig(kubernetes);
         return build(kubernetes.getAmazonEks().getUrl(), token);
     }
 
@@ -50,15 +50,20 @@ public class AmazonEksProvider {
                 //.withRequestTimeout(KubeClient.Config.REQUEST_TIMEOUT)
                 .withWatchReconnectInterval(60000)
                 .build();
+
         // 6.x 写法
-        return new KubernetesClientBuilder().withConfig(config).build();
+        //  return new KubernetesClientBuilder().withConfig(config).build();
+
+        // 5.x
+        return new DefaultKubernetesClient(config);
     }
 
     /**
      * 注入配置
+     *
      * @param kubernetes
      */
-    private static void preInitConfig(KubernetesConfig.Kubernetes kubernetes) {
+    private static void initConfig(KubernetesConfig.Kubernetes kubernetes) {
         System.setProperty(io.fabric8.kubernetes.client.Config.KUBERNETES_REQUEST_TIMEOUT_SYSTEM_PROPERTY, String.valueOf(KubeClient.Config.REQUEST_TIMEOUT));
         System.setProperty(io.fabric8.kubernetes.client.Config.KUBERNETES_WEBSOCKET_TIMEOUT_SYSTEM_PROPERTY, String.valueOf(KubeClient.Config.WEBSOCKET_TIMEOUT));
         System.setProperty(io.fabric8.kubernetes.client.Config.KUBERNETES_CONNECTION_TIMEOUT_SYSTEM_PROPERTY, String.valueOf(KubeClient.Config.CONNECTION_TIMEOUT));
