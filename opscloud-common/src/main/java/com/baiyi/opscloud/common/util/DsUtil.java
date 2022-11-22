c7844a4aab29fc33bfcd3cbc04e73263f1817da7
白衣 <ixrjog@qq.com>
2022-11-22 09:33:52 +0800

    修改Yaml序列化代码

--- opscloud-common/src/main/java/com/baiyi/opscloud/common/util/DsUtil.java
+++ opscloud-common/src/main/java/com/baiyi/opscloud/common/util/DsUtil.java
@@ -4,6 +4,7 @@ import com.baiyi.opscloud.common.exception.datasource.DatasourceRuntimeException
 import com.baiyi.opscloud.domain.ErrorEnum;
 import com.google.gson.JsonSyntaxException;
 import org.apache.commons.lang3.StringUtils;
+import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 import org.yaml.snakeyaml.representer.Representer;
@@ -18,14 +19,20 @@ public class DsUtil {
     private DsUtil() {
     }
 
+    /**
+     * https://stackabuse.com/reading-and-writing-yaml-files-in-java-with-snakeyaml/
+     * @param propsYml
+     * @param targetClass
+     * @return
+     * @param <T>
+     */
     public static <T> T toDsConfig(String propsYml, Class<T> targetClass) {
         if (StringUtils.isEmpty(propsYml))
             throw new DatasourceRuntimeException(ErrorEnum.DATASOURCE_PROPS_EMPTY);
         try {
-            Representer representer = new Representer();
+            Representer representer = new Representer(new DumperOptions());
             representer.getPropertyUtils().setSkipMissingProperties(true);
             Yaml yaml = new Yaml(new Constructor(targetClass),representer);
-            //Yaml yaml = new Yaml(loadingConfig);
             return yaml.loadAs(propsYml, targetClass);
         } catch (JsonSyntaxException e) {
             throw new DatasourceRuntimeException(ErrorEnum.DATASOURCE_PROPS_CONVERT_ERROR);
