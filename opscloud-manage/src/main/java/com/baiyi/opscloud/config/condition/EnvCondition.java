511e82533a331f5a88e1c561e3bcf50b48220b0d
修远 <448613222@qq.com>
2023-01-17 16:58:38 +0800

    新增 EnvCondition

--- opscloud-manage/src/main/java/com/baiyi/opscloud/config/condition/EnvCondition.java
+++ opscloud-manage/src/main/java/com/baiyi/opscloud/config/condition/EnvCondition.java
@@ -1,6 +1,23 @@
-package com.baiyi.opscloud.config.condition;/**
+package com.baiyi.opscloud.config.condition;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+import static com.baiyi.opscloud.common.base.Global.ENV_PROD;
+
+/**
  * @Author 修远
  * @Date 2023/1/17 11:13 AM
  * @Since 1.0
- */public class EnvCondition {
+ */
+@Slf4j
+public class EnvCondition implements Condition {
+
+    @Override
+    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        String env = context.getEnvironment().getActiveProfiles()[0];
+        return ENV_PROD.equals(env);
+    }
 }
