a061214102f380330761d451ceecd7b72300be1b
白衣 <ixrjog@qq.com>
2023-01-29 14:59:25 +0800

    修改readme

---
+++ opscloud-common/src/main/java/com/baiyi/opscloud/common/annotation/TaskWatch.java
@@ -0,0 +1,8 @@
+package com.baiyi.opscloud.common;
+/**
+ * @Author  baiyi
+ * @Date  2023/1/29 14:00
+ * @Version 1.0
+ */
+public class TaskWatch {
+}
---
+++ opscloud-common/src/main/java/com/baiyi/opscloud/common/aspect/TaskWatchAspect.java
@@ -0,0 +1,8 @@
+package com.baiyi.opscloud.common.aspect;
+/**
+ * @Author  baiyi
+ * @Date  2023/1/29 14:03
+ * @Version 1.0
+ */
+public class TaskWatchAspect {
+}
