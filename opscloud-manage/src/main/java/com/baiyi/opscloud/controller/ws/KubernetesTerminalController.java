68c9334c2c619a6e030c28b4728d8b4e7bb22ff9
白衣 <ixrjog@qq.com>
2023-02-14 11:04:44 +0800

    修改堡垒机maxMessageSize

--- opscloud-manage/src/main/java/com/baiyi/opscloud/controller/ws/KubernetesTerminalController.java
+++ opscloud-manage/src/main/java/com/baiyi/opscloud/controller/ws/KubernetesTerminalController.java
@@ -90,7 +90,7 @@ public class KubernetesTerminalController extends SimpleAuthentication {
      *
      * @param message 客户端发送过来的消息
      */
-    @OnMessage(maxMessageSize = 10 * 1024)
+    @OnMessage(maxMessageSize = 20 * 1024)
     public void onMessage(String message, Session session) {
         if (!session.isOpen() || StringUtils.isEmpty(message)) return;
         String state = getState(message);
