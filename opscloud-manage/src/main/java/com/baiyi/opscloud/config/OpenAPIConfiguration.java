17d80193ba8a00958a6b26fd12475801f7c757a1
白衣 <ixrjog@qq.com>
2023-04-06 17:49:55 +0800

    修改OpenAPI

--- opscloud-manage/src/main/java/com/baiyi/opscloud/config/OpenAPIConfiguration.java
+++ opscloud-manage/src/main/java/com/baiyi/opscloud/config/OpenAPIConfiguration.java
@@ -1,7 +1,10 @@
 package com.baiyi.opscloud.config;
 
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,8 +16,12 @@ import org.springframework.context.annotation.Configuration;
  * @Date 2023/4/6 13:47
  * @Version 1.0
  */
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @Configuration
 public class OpenAPIConfiguration {
+
+    @Value("${spring.application.name}")
+    private String name;
 
     @Value("${spring.application.version}")
     private String version;
@@ -25,11 +32,17 @@ public class OpenAPIConfiguration {
         License license = new License().name("APACHE LICENSE, VERSION 2.0")
                 .url("https://www.apache.org/licenses/LICENSE-2.0");
 
+        Contact contact = new Contact()
+                .url("https://github.com/ixrjog/opscloud4")
+                .name("Jan")
+                .email("ixrjog@qq.com");
+
         return new OpenAPI()
                 .info(new Info()
-                        .title(String.format("Opscloud %s", version))
-                        .description("Opscloud OpenAPI接口文档")
+                        .title(name.toUpperCase())
                         .version(version)
+                        .description("The Open API")
+                        .contact(contact)
                         .license(license));
     }
 
