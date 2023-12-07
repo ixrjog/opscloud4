1f67a936da0db5b4a365ecf9aee864a91de92646
白衣 <ixrjog@qq.com>
2023-12-07 10:49:02 +0800

    虚拟线程初始化堡垒机

--- opscloud-terminal/src/main/java/com/baiyi/opscloud/terminal/handler/impl/ServerTerminalLoginHandler.java
+++ opscloud-terminal/src/main/java/com/baiyi/opscloud/terminal/handler/impl/ServerTerminalLoginHandler.java
@@ -17,9 +17,10 @@ import com.google.gson.GsonBuilder;
 import jakarta.annotation.Resource;
 import jakarta.websocket.Session;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * 登录
@@ -38,8 +39,8 @@ public class ServerTerminalLoginHandler extends AbstractServerTerminalHandler<Se
     @Resource
     private ServerService serverService;
 
-    @Autowired
-    private ThreadPoolTaskExecutor serverTerminalExecutor;
+//    @Autowired
+//    private ThreadPoolTaskExecutor serverTerminalExecutor;
 
     @Override
     public String getState() {
@@ -48,12 +49,13 @@ public class ServerTerminalLoginHandler extends AbstractServerTerminalHandler<Se
 
     @Override
     public void handle(String message, Session session, TerminalSession terminalSession) {
-        try {
+        // 使用虚拟线程处理
+        try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()){
             ServerMessage.Login loginMessage = toMessage(message);
             heartbeat(terminalSession.getSessionId());
             String username = SessionHolder.getUsername();
             for (ServerNode serverNode : loginMessage.getServerNodes()) {
-                serverTerminalExecutor.submit(() -> {
+                executor.submit(() -> {
                     SessionHolder.setUsername(username);
                     log.info("Login server: instanceId={}", serverNode.getInstanceId());
                     superAdminInterceptor.interceptLoginServer(serverNode.getId());
